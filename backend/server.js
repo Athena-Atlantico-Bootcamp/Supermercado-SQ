5c14792c6cbc56c89f655b7c8e72f435ea498dad
beatrizmakowski <bemakow@gmail.com>
2022-06-05 17:03:53 -0300

    feat(docs): adicionando configuração do Swagger e rota /docs

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,42 @@
 'use strict'
 import express from 'express'
 import routes from '../backend/src/routes'
+import swaggerJSDoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
 
 const app = express()
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        swagger: '2.0',
+        info: {
+            title: 'Supermercado SQ',
+            version: '1.0.0',
+            description: "Documentação para utilização da API \"Supermercado SQ\"."
+        },
+        host: 'http://localhost:3000',
+        basepath: '/',
+        tags: [
+            {
+                name: 'usuarios',
+                description: 'Tudo sobre nossos usuários'
+            },
+            {
+                name: 'produtos',
+                description: 'Tudo sobre nossos produtos'
+            },
+            {
+                name: 'comentarios',
+                description: 'Tudo sobre os comentários realizados no site'
+            },
+        ],
+        schemes: 'http'
+    },
+    apis: ['./src/**/*-routes.js']
+}
+const swaggerDocs = swaggerJSDoc(swaggerOptions)
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+
 
 app.use(express.json())
 
