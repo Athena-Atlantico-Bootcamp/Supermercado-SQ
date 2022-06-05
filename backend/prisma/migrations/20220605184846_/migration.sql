8234fef9793918b488847442a8649f4744621a3e
beatrizmakowski <bemakow@gmail.com>
2022-06-05 15:51:10 -0300

    fet: prisma migrate

---
+++ backend/prisma/migrations/20220605184846_/migration.sql
@@ -0,0 +1,45 @@
+-- CreateTable
+CREATE TABLE "usuarios" (
+    "id_usuario" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "nome" TEXT NOT NULL,
+    "telefone" INTEGER NOT NULL,
+    "restricoes" TEXT NOT NULL,
+    "tipo_usuario" TEXT NOT NULL,
+
+    CONSTRAINT "usuarios_pkey" PRIMARY KEY ("id_usuario")
+);
+
+-- CreateTable
+CREATE TABLE "produtos" (
+    "id_produto" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+    "descricao" TEXT NOT NULL,
+    "imagem" TEXT NOT NULL,
+    "ingredientes" TEXT NOT NULL,
+    "usuarioId" INTEGER NOT NULL,
+
+    CONSTRAINT "produtos_pkey" PRIMARY KEY ("id_produto")
+);
+
+-- CreateTable
+CREATE TABLE "comentarios" (
+    "id_comentario" SERIAL NOT NULL,
+    "texto_comentario" TEXT NOT NULL,
+    "usuarioId" INTEGER NOT NULL,
+    "produtoId" INTEGER NOT NULL,
+
+    CONSTRAINT "comentarios_pkey" PRIMARY KEY ("id_comentario")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "usuarios_email_key" ON "usuarios"("email");
+
+-- AddForeignKey
+ALTER TABLE "produtos" ADD CONSTRAINT "produtos_usuarioId_fkey" FOREIGN KEY ("usuarioId") REFERENCES "usuarios"("id_usuario") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "comentarios" ADD CONSTRAINT "comentarios_usuarioId_fkey" FOREIGN KEY ("usuarioId") REFERENCES "usuarios"("id_usuario") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "comentarios" ADD CONSTRAINT "comentarios_produtoId_fkey" FOREIGN KEY ("produtoId") REFERENCES "produtos"("id_produto") ON DELETE RESTRICT ON UPDATE CASCADE;
