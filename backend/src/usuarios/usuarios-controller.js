480f2770fbe138d5e46a3ace412d86a552ee81d9
Luiza Gadelha <89354119+LuizaGA@users.noreply.github.com>
2022-06-16 17:53:31 -0300

    fix(usuarios): adicionando try/catch

--- backend/src/usuarios/usuarios-controller.js
+++ backend/src/usuarios/usuarios-controller.js
@@ -6,42 +6,44 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
 class UsuarioController {
-
     async createUser(req, res) { 
         const {nome, email, senha, telefone, restricoes, tipo_usuario} = req.body
-        const senhaHash = await bcrypt.hash(senha, 8)
-        const usuario = await prisma.usuarios.create({
-            data: {
-                nome: nome,
-                email: email,
-                senha: senhaHash,
-                telefone: telefone,
-                restricoes: restricoes,
-                tipo_usuario: tipo_usuario
-            }
-        })
-        return res.status(200).json(usuario)
+        try{
+            const senhaHash = await bcrypt.hash(senha, 8)
+            const usuario = await prisma.usuarios.create({
+                data: {
+                    nome: nome,
+                    email: email,
+                    senha: senhaHash,
+                    telefone: telefone,
+                    restricoes: restricoes,
+                    tipo_usuario: tipo_usuario
+                }
+            })
+            return res.status(200).json(usuario)
+        }catch(e){
+            return res.status(400).json(`Informação inválida`)
+        }
     }
 
     async login(req,res){
         try{
             const {email, senha} = req.body
-        const userEmail = await prisma.usuarios.findUnique({ where: { email }})
-        if (!userEmail) {
-            return res.status(400).json('Email não cadastrado')
-        } 
-        const compararSenha = await bcrypt.compare(senha, userEmail.senha)
-        if(!compararSenha){
-            return res.status(400).json('Email e/ou senha invalido')
-        }
-        return res.status(200).json({
-            userEmail,
-            token:jwt.sign({
-                id: userEmail.id
-            },  process.env.SECRET, { expiresIn: '1h'})
-        })
+            const userEmail = await prisma.usuarios.findUnique({ where: { email }})
+            if (!userEmail) {
+                return res.status(400).json('Email não cadastrado')
+            } 
+            const compararSenha = await bcrypt.compare(senha, userEmail.senha)
+            if(!compararSenha){
+                return res.status(400).json('Email e/ou senha invalido')
+            }
+            return res.status(200).json({
+                userEmail,
+                token:jwt.sign({
+                    id: userEmail.id
+                },  process.env.SECRET, { expiresIn: '1h'})
+            })
         }catch(e){
-            console.log(e)
             return res.status(400).json('Erro')
         }
     }
@@ -52,55 +54,60 @@ class UsuarioController {
     }
 
     async showUserById(id_usuario, res) {
-
-        // Verifica se o usuário com o id especificado existe.
-        let userFlag = await this.checkIfUserExists(id_usuario)
-        // Caso não exista, retorna status 404
-        if (!userFlag) {
-            return res.status(404).json('Usuário não encontrado.')
-        }
-        // Caso exista, retorna status 200 + o usuário
-        const usuario = await prisma.usuarios.findUnique({
-            where: {
-                id_usuario: id_usuario
+        try{
+             // Verifica se o usuário com o id especificado existe.
+            let userFlag = await this.checkIfUserExists(id_usuario)
+            // Caso não exista, retorna status 404
+            if (!userFlag) {
+                return res.status(404).json('Usuário não encontrado.')
             }
-        })
-        return res.status(200).json(usuario)
+            // Caso exista, retorna status 200 + o usuário
+            const usuario = await prisma.usuarios.findUnique({
+                where: {
+                    id_usuario: id_usuario
+                }
+            })
+            return res.status(200).json(usuario)
+        }catch(e){
+            return res.status(400).json('Informação inválida')
+        }
     }
 
     async updateUser(id_usuario, req, res) {
-        
-        // Verifica se o usuário com o id especificado existe.
-        let userFlag = await this.checkIfUserExists(id_usuario)
-        // Caso não exista, retorna status 404
-        if (!userFlag) {
-            return res.status(404).json('Usuário não encontrado.')
+        try{
+            let userFlag = await this.checkIfUserExists(id_usuario)        
+            if (!userFlag) {
+                return res.status(404).json('Usuário não encontrado.')
+            }
+            // Caso exista, atualiza o usuário
+            const usuarioAtualizado = await prisma.usuarios.update({
+                where: {
+                    id_usuario: id_usuario
+                },
+                data: req.body
+            })
+            return res.status(200).json(usuarioAtualizado)
+        }catch(e){
+            return res.status(400).json('Informação inválida')
         }
-        // Caso exista, atualiza o usuário
-        const usuarioAtualizado = await prisma.usuarios.update({
-            where: {
-                id_usuario: id_usuario
-            },
-            data: req.body
-        })
-        return res.status(200).json(usuarioAtualizado)
     }
 
     async deleteUser(id_usuario, res) {
-
-        // Verifica se o usuário com o id especificado existe.
-        let userFlag = await this.checkIfUserExists(id_usuario)
-        // Caso não exista, retorna status 404
-        if (!userFlag) {
-            return res.status(404).json('Usuário não encontrado.')
-        }
-        // Caso exista, deleta o usuário e retorna status 200
-        const deleteUser = await prisma.usuarios.delete({
-            where: {
-              id_usuario: id_usuario
+        try {
+            let userFlag = await this.checkIfUserExists(id_usuario)
+            if (!userFlag) {
+                return res.status(404).json('Usuário não encontrado.')
             }
-        })
-        return res.status(200).json('Usuário deletado com sucesso.')
+            // Caso exista, deleta o usuário e retorna status 200
+            const deleteUser = await prisma.usuarios.delete({
+                where: {
+                id_usuario: id_usuario
+                }
+            })
+            return res.status(200).json('Usuário deletado com sucesso.')
+        } catch (e) {
+            return res.status(400).json('Informação inválida')
+        }
     }
 
     /**
@@ -115,14 +122,12 @@ class UsuarioController {
                 id_usuario: id_usuario
             }
         })
-        
         if (!user) {
             return false
         } else {
             return true
         }
     }
-
 }
 
 export default UsuarioController
