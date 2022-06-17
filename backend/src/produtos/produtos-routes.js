b697887b3eec4f11167efd18e423a94bcb0461ee
Luiza Gadelha <luizagadelha@unifor.com>
2022-06-17 17:09:55 -0300

    fix(produto): documentação das rotas

--- backend/src/produtos/produtos-routes.js
+++ backend/src/produtos/produtos-routes.js
@@ -9,6 +9,38 @@ const produtoController = new ProdutoController()
 
 /**
  * @swagger
+ * components:
+ *   schemas:
+ *       produtos:
+ *         type: object
+ *         required:
+ *         - id_produto
+ *         - nome
+ *         - ingredientes
+ *         - usuarioId
+ *         properties:
+ *           id_produto:
+ *             type: serial
+ *             description: ID serial gerado automaticamente pelo banco de dados
+ *           nome:
+ *             type: string
+ *             description: Nome do produto
+ *           descricao:
+ *             type: string
+ *             description: Descrição do produto
+ *           imagem:
+ *             type: string
+ *             description: Imagem do produto
+ *           ingredientes:
+ *             type: string
+ *             description: Ingredientes do produto
+ *           usuarioId:
+ *             type: integer
+ *             description: Id do usuário que cadastrou o produto
+ */
+
+/**
+ * @swagger
  * /produtos:
  *   post:
  *     tags:
@@ -33,7 +65,7 @@ const produtoController = new ProdutoController()
  *           nome:
  *             type: string
  *             description: Nome do produto
- *           descricao: 
+ *           descricao:
  *             type: string
  *             description: Descrição do produto
  *           imagem:
@@ -53,40 +85,198 @@ const produtoController = new ProdutoController()
  *           404:
  *             description: Usuário não encontrado
  */
-router.post('/',autentificacao, (req, res, next) => {
+router.post('/', autentificacao, (req, res, next) => {
   return produtoController.createProduct(req, res)
 })
+
+/**
+ * @swagger
+ * /produtos:
+ *   get:
+ *     tags:
+ *     - produtos
+ *     summary: Mostra todos os produtos cadastrados
+ *     description: ""
+ *     parameters: ""
+ *     responses:
+ *       200:
+ *         description: OK
+ *       500:
+ *         description: Internal Server Error
+ */
 
 router.get('/', (req, res, next) => {
   return produtoController.showAllProducts(req, res)
 })
+
+/**
+ * @swagger
+ * /produtos/{id}:
+ *   get:
+ *     tags:
+ *     - produtos
+ *     summary: Mostra um produtos especificado pelo seu id
+ *     description: ""
+ *     parameters:
+ *     - name: id_produtos
+ *       in: path
+ *       required: true
+ *       description: O id do produtos a ser pesquisado
+ *       type: integer
+ *     responses:
+ *       200:
+ *         description: OK
+ *       400:
+ *         description: Informação invalída
+ *       500:
+ *         description: Internal Server Error
+ */
 
 router.get('/:id', (req, res, next) => {
   const id_produto = parseInt(req.params.id)
   return produtoController.showProductById(id_produto, res)
 })
 
-router.get('/usuario/:id',autentificacao, (req, res, next) => {
+/**
+ * @swagger
+ * /produtos/usuario/{id}:
+ *   get:
+ *     tags:
+ *     - produtos
+ *     summary: Mostra os produtos criados pelo id do usuário
+ *     description: ""
+ *     parameters:
+ *     - name: id_usuario
+ *       in: path
+ *       required: true
+ *       description: O id do usuario que criou os produtos procurados
+ *       type: integer
+ *     responses:
+ *       200:
+ *         description: OK
+ *       400:
+ *         description: Informação invalída
+ *       500:
+ *         description: Internal Server Error
+ */
+
+router.get('/usuario/:id', autentificacao, (req, res, next) => {
   const id_usuario = parseInt(req.params.id)
   return produtoController.showAllProductByUserId(id_usuario, res)
 })
+
+/**
+ * @swagger
+ * /produtos/ingrediente/{ingrediente}:
+ *   get:
+ *     tags:
+ *     - produtos
+ *     summary: Mostra os produtos que contém o ingrediente pesquisado
+ *     description: ""
+ *     parameters:
+ *     - name: ingrediente
+ *       in: path
+ *       required: true
+ *       description: O ingrediente que você deseja pesquisar os produtos que o apresentem
+ *       type: string
+ *     responses:
+ *       200:
+ *         description: OK
+ *       400:
+ *         description: Informação invalída
+ *       500:
+ *         description: Internal Server Error
+ */
 
 router.get('/ingrediente/:ingrediente', (req, res, next) => {
   const ingrediente = req.params.ingrediente
   return produtoController.showAllProductsByIngredient(ingrediente, res)
 })
 
+/**
+ * @swagger
+ * /produtos/semingrediente/{ingrediente}:
+ *   get:
+ *     tags:
+ *     - produtos
+ *     summary: Mostra os produtos que não contém o ingrediente pesquisado
+ *     description: ""
+ *     parameters:
+ *     - name: ingrediente
+ *       in: path
+ *       required: true
+ *       description: O ingrediente que você deseja pesquisar os produtos não que o apresentem
+ *       type: string
+ *     responses:
+ *       200:
+ *         description: OK
+ *       400:
+ *         description: Informação invalída
+ *       500:
+ *         description: Internal Server Error
+ */
+
 router.get('/semingrediente/:ingrediente', (req, res, next) => {
   const ingrediente = req.params.ingrediente
   return produtoController.showAllProductsWithoutIngredient(ingrediente, res)
 })
 
-router.patch('/:id',autentificacao, (req, res, next) => {
+/**
+ * @swagger
+ * /produtos/{id}:
+ *   patch:
+ *     tags:
+ *     - produtos
+ *     summary: Altera um produto especificado pelo seu id
+ *     description: ""
+ *     parameters:
+ *     - name: id_produto
+ *       in: path
+ *       required: true
+ *       description: O id do produto a ser atualizado
+ *       type: integer
+ *     - body: body
+ *       in: body
+ *       required: true
+ *       description: O json contendo os campos e valores a serem atualizados
+ *     responses:
+ *       200:
+ *         description: OK
+ *       400:
+ *         description: Informação invalída
+ *       500:
+ *         description: Internal Server Error
+ */
+
+router.patch('/:id', autentificacao, (req, res, next) => {
   const id_produto = parseInt(req.params.id)
   return produtoController.updateProduct(id_produto, req, res)
 })
 
-router.delete('/:id',autentificacao, (req, res, next) => {
+/**
+ * @swagger
+ * /produtos/{id}:
+ *   delete:
+ *     tags:
+ *     - produtos
+ *     summary: Deleta um produto especificado pelo seu id
+ *     description: ""
+ *     parameters:
+ *     - name: id_produto
+ *       in: path
+ *       required: true
+ *       description: O id do produto a ser deletado
+ *       type: integer
+ *     responses:
+ *       200:
+ *         description: OK
+ *       404:
+ *         description: Usuário não encontrado
+ *       500:
+ *         description: Internal Server Error
+ */
+
+router.delete('/:id', autentificacao, (req, res, next) => {
   const id_produto = parseInt(req.params.id)
   return produtoController.deleteProduct(id_produto, res)
 })
