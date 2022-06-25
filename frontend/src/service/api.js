cedbcefbbd56aeb90d91401ec46f37d8c1e49e0f
beatrizmakowski <bemakow@gmail.com>
2022-06-24 21:33:47 -0300

    feat(integração): conexão frontend com servidor local via cors

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -1,7 +1,9 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "https://supermercado-athena.herokuapp.com"
+    //baseURL: "https://supermercado-athena.herokuapp.com"
+    baseURL: "http://localhost:3001"
+
 })
 
 export default api
