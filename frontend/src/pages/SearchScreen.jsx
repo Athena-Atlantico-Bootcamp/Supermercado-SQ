c5bf8d79b8ea42a81a5bfad84d19aae4b9667e0d
beatrizmakowski <bemakow@gmail.com>
2022-06-24 21:47:48 -0300

    feat(frontend): integrando rota get /produtos via heroku

--- frontend/src/pages/SearchScreen.jsx
+++ frontend/src/pages/SearchScreen.jsx
@@ -1,21 +1,30 @@
 import api from '../service/api'
+import { useState, useEffect } from 'react'
 
 function SearchScreen() {
+
+  const [products, setProducts] = useState([])
 
   function getProducts() {
     try {
       api.get("/produtos")
-      .then( (res) => {console.log(res.data)} )
+      .then( (res) => {
+        console.log(res.data)
+        setProducts(res.data)
+      })
     } catch (err) {
       console.log(err.message)
     }
   }
 
-  getProducts()
+  useEffect(() => {getProducts()}, [])
 
   return (
     <div>
       <p>Tela de Busca</p>
+      { products ? products.map((p) => {
+        return <div>{ p.nome }</div>
+      }) : <div>oi</div>}
     </div>
   )
 }
