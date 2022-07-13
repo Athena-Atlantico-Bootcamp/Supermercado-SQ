526b219ff6f594b19031356c3c1b652dd6f1f1bb
joaovictorgit <victoralmeida.0216@gmail.com>
2022-07-13 14:30:02 -0300

    (feature/testeLogin) - Correções das páginas

---
+++ frontend/src/pages/App/index.jsx
@@ -0,0 +1,43 @@
+import {useState, useEffect} from 'react'
+import Header from '../../components/Header/Header'
+import Footer from '../../components/Footer'
+import CardProdutos from '../../components/CardProdutos'
+import api from '../../service/api'
+import { Container, ContainerTitulo, Titulo, ContainerProdutos } from './styles'
+
+function App() {
+  const [products, setProducts] = useState([])
+
+  function getProducts() {
+    try {
+      api.get("/produtos")
+      .then( (res) => {
+        setProducts(res.data)
+      })
+    } catch (err) {
+      console.log(err.message)
+    }
+  }
+  useEffect(() => {getProducts()}, [])
+  
+  return (
+    <div>
+      <Header/>
+      <Container>
+        <ContainerTitulo>
+          <Titulo> Adicionados Recentemente</Titulo>
+        </ContainerTitulo>
+        <ContainerProdutos>
+          { products ? products.map((p) => {
+            return (
+              <CardProdutos data={p} key={p.id_produto}/>
+              )
+          }) : null}
+        </ContainerProdutos>
+      </Container>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
