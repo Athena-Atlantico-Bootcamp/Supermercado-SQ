9e8a7b716d7a79a86433935ee34588f810cf700d
beatrizmakowski <bemakow@gmail.com>
2022-07-13 21:23:41 -0300

    fix: removendo comentários desnecessários do arquivo Header.jsx

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -7,12 +7,6 @@ const Header = () => {
       <LinkWrapper to="/" >
         <NomeSupermercado>Supermercado SQ</NomeSupermercado>
       </LinkWrapper>
-      {/* <ul style={{ listStyleType: "none" }}>
-        <li><LinkWrapper to='/buscar'>Buscar</LinkWrapper></li>
-        <li><LinkWrapper to='/sobre-nos'>Sobre nós</LinkWrapper></li>
-        <li><LinkWrapper to='/alergias'>Alergias Alimentares</LinkWrapper></li>
-        <li><LinkWrapper to='/login'>Login</LinkWrapper></li>
-      </ul> */}
       <List>
         <li><LinkWrapper to='/buscar'>Buscar</LinkWrapper></li>
         <li><LinkWrapper to='/sobre-nos'>Sobre nós</LinkWrapper></li>
