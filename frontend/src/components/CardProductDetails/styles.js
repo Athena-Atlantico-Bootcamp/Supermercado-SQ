3a2ad452d207c1c85955585c8283432b50e49791
beatrizmakowski <bemakow@gmail.com>
2022-07-14 16:07:17 -0300

    feat: ajustes CSS parte 1

--- frontend/src/components/CardProductDetails/styles.js
+++ frontend/src/components/CardProductDetails/styles.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div` 
 width: 100%;
 box-sizing: border-box;
-padding: 4rem 8rem 4rem 5rem;
+padding: 4rem 5rem 4rem 5rem;
 
 @media only screen and (max-width: 1070px) {
     padding: 4rem;
@@ -12,6 +12,7 @@ padding: 4rem 8rem 4rem 5rem;
 @media only screen and (max-width: 768px) {
     padding: 1rem;
 }
+
 `
 
 
@@ -39,17 +40,29 @@ export const ContainerCard = styled.div`
     
 `
 
+export const ContainerTitle = styled.div` 
+    display: flex;
+    flex-wrap: wrap;
+`
+
+export const ContainerProdutoTitle = styled.div`
+    padding-right: 30%;
+`
+
 
 export const ContainerColumn = styled.div` 
-    display:flex;
+    display: flex;
+    border-radius: 25px;
 
+    border: solid 1px blue;
 `
 
 
 export const ContainerImg = styled.div` 
-width: 400px;
-height: 400px;
-background-color: grey;
+    width: 400px;
+    height: 400px;
+    background-color: transparent;
+    border-top-right-radius: 25px;
 
 
 `
@@ -57,15 +70,21 @@ background-color: grey;
 export const Img = styled.img`
     width: 100%;
     height: 100%;
-    
+    border-top-left-radius: 15px;
+    border-bottom-left-radius: 15px;
+    object-fit: contain;
     
 `;
 
 export const ContainerDescription = styled.div`
-    width: 700px;
-    height: 400px;
+    width: calc(100vw - 400px);
+    min-height: 400px;
     background-color: white;
     overflow-y: scroll;
+    border-top-right-radius: 15px;
+    border-bottom-right-radius: 15px;
+
+    border: solid 1px red;
 `
 
 
