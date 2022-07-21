370af5e01c35a37987e26d20235c8386595c4219
Luiza Gadelha <luizagadelha@unifor.com>
2022-07-21 15:24:35 -0300

    fix(footer): navegação

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -1,8 +1,14 @@
 import { useNavigate } from 'react-router-dom';
 import { Container, NomeSupermercado, Texto, Link, ContainerTexto } from './styles'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Footer() {
   const navigate = useNavigate();
+
+  function paginaHome() {
+    navigate("/");
+  }
 
   function paginaQuemSomos() {
     navigate("/sobre-nos");
@@ -13,20 +19,30 @@ function Footer() {
   }
 
   function paginaLogin() {
-    navigate("/perfil");
+    const tipoUsuario = JSON.parse(localStorage.getItem('@tipo_usuario'))
+    console.log('tipo ',tipoUsuario)
+    console.log(tipoUsuario == 'usuario')
+    if(tipoUsuario == 'usuario' || tipoUsuario == 'fornecedor'){
+      navigate("/perfil");
+    } else if (tipoUsuario){
+      navigate("/perfil-adm");
+    } else {
+      navigate("/login");
+    }
   }
 
   return (
     <Container>
-      <NomeSupermercado>Supermercado SQ</NomeSupermercado>
+      <ToastContainer />
+      <NomeSupermercado><Link href="" onClick={paginaQuemSomos}>Supermercado SQ</Link></NomeSupermercado>
       <ContainerTexto>
         <Texto>INSTITUCIONAL</Texto>
         <Texto><Link href="" onClick={paginaQuemSomos}>Quem somos</Link></Texto>
       </ContainerTexto>
       <ContainerTexto>
         <Texto>CONTA</Texto>
-        <Texto><Link href="paginaCriarConta" onClick={paginaCriarConta}>Criar uma conta</Link></Texto>
-        <Texto><Link href="paginaLogin" onClick={paginaLogin}>Minha conta</Link></Texto>
+        <Texto><Link href="" onClick={paginaCriarConta}>Criar uma conta</Link></Texto>
+        <Texto><Link href="" onClick={paginaLogin}>Minha conta</Link></Texto>
       </ContainerTexto>
       <div>
         <Link href="" onClick={paginaQuemSomos}>
